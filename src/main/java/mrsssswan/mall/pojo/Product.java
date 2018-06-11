18a5627d94395dd4590b3e5359c2943ce53f4fa6
Mrsssswan <1520010400@qq.com>
2018-06-11 17:10:00 +0800

    init finished

--- src/main/java/mrsssswan/mall/pojo/Product.java
+++ src/main/java/mrsssswan/mall/pojo/Product.java
@@ -14,6 +14,10 @@ public class Product {
 
     private String mainImage;
 
+    private String subImages;
+
+    private String detail;
+
     private BigDecimal price;
 
     private Integer stock;
@@ -24,12 +28,14 @@ public class Product {
 
     private Date updateTime;
 
-    public Product(Integer id, Integer categoryId, String name, String subtitle, String mainImage, BigDecimal price, Integer stock, Integer status, Date createTime, Date updateTime) {
+    public Product(Integer id, Integer categoryId, String name, String subtitle, String mainImage, String subImages, String detail, BigDecimal price, Integer stock, Integer status, Date createTime, Date updateTime) {
         this.id = id;
         this.categoryId = categoryId;
         this.name = name;
         this.subtitle = subtitle;
         this.mainImage = mainImage;
+        this.subImages = subImages;
+        this.detail = detail;
         this.price = price;
         this.stock = stock;
         this.status = status;
@@ -79,6 +85,22 @@ public class Product {
 
     public void setMainImage(String mainImage) {
         this.mainImage = mainImage == null ? null : mainImage.trim();
+    }
+
+    public String getSubImages() {
+        return subImages;
+    }
+
+    public void setSubImages(String subImages) {
+        this.subImages = subImages == null ? null : subImages.trim();
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail == null ? null : detail.trim();
     }
 
     public BigDecimal getPrice() {
